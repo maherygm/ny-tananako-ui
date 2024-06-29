5ef0cb4af120e8bb829c3a1a7d7e0b4a355cf422
mahery <maheryrak1234@gmail.com>
2024-06-29 21:13:20 +0300

    first commit

---
+++ src/components/maps/maps/Maps.jsx
@@ -0,0 +1,48 @@
+import L from "leaflet";
+import "leaflet-control-geocoder/dist/Control.Geocoder.css";
+import "leaflet-control-geocoder/dist/Control.Geocoder.js";
+import "leaflet/dist/leaflet.css";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer
+} from "react-leaflet";
+import LeafletRoutingMachine from "../LeafletRoutingMachine";
+
+
+const VIsite = () => {
+  const position = [-21.43772, 47.09989];
+  return (
+    <div className="w-full h-screen overflow-hidden z-0">
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ minHeight: "100vh", minWidth: "100vw" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+
+        <LeafletRoutingMachine />
+      </MapContainer>
+    </div>
+  );
+};
+
+export default VIsite;
+
+let DefaultIcon = L.icon({
+  iconUrl: "/marker-icon.png",
+  iconSize: [25, 41],
+  iconAnchor: [10, 41],
+  popupAnchor: [2, -40],
+});
+L.Marker.prototype.options.icon = DefaultIcon;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,10 @@
+import Maps from "../components/maps/maps/Maps"
+const Main = () => {
+    return (
+        <div>
+            <Maps />
+        </div>
+    )
+}
+
+export default Main
