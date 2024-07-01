0b5ed9863df59c045c531903372a9fcc7e79b9f0
mahery <maheryrak1234@gmail.com>
2024-07-01 23:41:25 +0300

    feat login finished

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
 
     if (user.role === "admin") {
         return <Admin />
-    } else if (user.role === "client") {
+    } else if (user.role === "clients") {
         return <Client />
     }
     return null;
